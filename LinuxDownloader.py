e62cbe0ca275e0f41f233c253beacae890a58201
Hovakim Grabski <hovakim_grabski@yahoo.com>
2020-08-15 23:03:17 +0400

    create folder kinda works

--- LinuxDownloader.py
+++ LinuxDownloader.py
@@ -1,15 +1,37 @@
 import yaml
+import sys
+import os
+
+def create_folder(dirName):
+    # Create target Directory if don't exist
+    if not os.path.exists(dirName):
+        os.mkdir(dirName)
+        print("Directory ", dirName, " Created ")
+    else:
+        print("Directory ", dirName, " already exists")
+
 
 with open('example.yml') as f:
     # data = yaml.load(f, Loader=yaml.FullLoader)
     data = yaml.load(f)
     print(data)
 
-    sub_stuff = data[0]['Folders']
+    try:
+        sub_stuff = data[0]['Folders']
+    except Exception as e:
+        print('Please follow the official example.yml file')
+        sys.exit(0)
+
+
     print(sub_stuff)
-    # for doc in data:
-    #     for k, v in doc.items():
-    #         print(k, "->", v)
+
+    for folder in sub_stuff:
+        print('Folder is: ', folder)
+
+        folder_to_create = folder['name']
+        create_folder(folder_to_create)
+
+        print('----------------------\n')
 
 
 test = 1
