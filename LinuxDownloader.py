45ddf2cb9d33c2f03d430e6bd01d17b40e3d6338
Hovakim Grabski <hovakim_grabski@yahoo.com>
2020-08-15 23:17:53 +0400

    strip filename

--- LinuxDownloader.py
+++ LinuxDownloader.py
@@ -12,13 +12,31 @@ def create_folder(dirName):
         print("Directory ", dirName, " already exists")
 
 
-def download_file_aria2c(afile):
+def strip_filename(url):
+    path = url
+    firstpos = path.rfind("/")
+    lastpos = len(path)
+    filename = path[firstpos + 1:lastpos]
+    path = filename
+    if 'torrent' in filename:
+        firstpos = 0
+        lastpos = path.rfind(".torrent")
+        filename = path[firstpos:lastpos]
+    return filename
+
+test = 'https://pop-iso.sfo2.cdn.digitaloceanspaces.com/20.04/amd64/nvidia/11/pop-os_20.04_amd64_nvidia_11.iso.torrent'
+test1 = strip_filename(test)
+print(test1)
+
+
+def download_file_aria2c(url, download_folder):
     # download (using aria2c) files
+    afile = ''
     if os.path.exists(afile) and not os.path.exists(afile + '.aria2'):
         print('Skipping already-retrieved file: ' + afile)
     else:
         print('Downloading file: ' + afile)
-        subprocess.Popen(["aria2c", "-x", "16", "-s", "20", str(afile)]).wait()
+        subprocess.Popen(["aria2c", "-x", "16", "-s", "20", "-d", "{}".format(download_folder), str(afile)]).wait()
 
 
 with open('example.yml') as f:
@@ -42,6 +60,7 @@ with open('example.yml') as f:
         create_folder(folder_to_create)
 
         print('----------------------\n')
+
 
 
 test = 1
